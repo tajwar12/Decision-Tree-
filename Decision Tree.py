aa9211a4d79f7799c8bba8fb2a478adde08a48fd
Muhammad Tajwar <80433157+tajwar12@users.noreply.github.com>
2021-05-08 04:12:45 +0200

    Add files via upload

---
+++ Decision Tree.py
@@ -0,0 +1,104 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 14 16:38:47 2019
+
+@author: Tajwar
+"""
+
+# Load libraries
+import pandas
+from pandas.plotting import scatter_matrix
+import matplotlib.pyplot as plt
+from sklearn import model_selection
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+
+
+# Load dataset
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
+names = ['DataPlus', 'DualPane','ExternalQuiz','Folder','Forumng','Glossary','Homepage','HtmlActivity','OuCollaborate','Oucontent','OuElluminate','OuWiki','Page','Questionnaire','Quiz','RepeatActivity','Resource','SharedSubpage','SubPage','Url','final_result']
+dataset = pandas.read_csv("38Weeks-WithPass-Data.csv")
+
+# shape
+print(dataset.shape)
+
+# head
+print(dataset.head(20))
+
+# descriptions
+print(dataset.describe())
+
+# class distribution
+print(dataset.groupby('final_result').size())
+
+#box and whisker plots
+
+
+#dataset.plot(kind='box', subplots=True, layout=(21,21), sharex=False, sharey=False)
+#plt.show()
+#
+### histograms
+#dataset.hist()
+#plt.show()
+#
+### scatter plot matrix
+#scatter_matrix(dataset)
+#plt.show()
+
+# Split-out validation dataset
+array = dataset.values
+X = array[:,0:20]
+Y = array[:,21]
+validation_size = 0.20
+seed = 7
+X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size, random_state=seed)
+
+# Test options and evaluation metric
+seed = 7
+scoring = 'accuracy'
+
+#
+## Spot Check Algorithms
+models = []
+models.append(('LR', LogisticRegression(solver='liblinear', multi_class='ovr')))
+#models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC(gamma='auto')))
+# evaluate each model in turn
+results = []
+names = []
+for name, model in models:
+    kfold = model_selection.KFold(n_splits=10, random_state=None)
+    cv_results = model_selection.cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
+    results.append(cv_results)
+    names.append(name)
+    msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+    print(msg)
+#    
+## Compare Algorithms
+fig = plt.figure()
+fig.suptitle('Algorithm Comparison')
+ax = fig.add_subplot(222)
+plt.boxplot(results)
+ax.set_xticklabels(names)
+plt.show()
+#
+## Make predictions on validation dataset
+knn = KNeighborsClassifier()
+knn.fit(X_train, Y_train)
+predictions = knn.predict(X_validation)
+
+print("---------Accuracy--------")
+
+print(accuracy_score(Y_validation, predictions))
+print(confusion_matrix(Y_validation, predictions))
+print(classification_report(Y_validation, predictions))    
